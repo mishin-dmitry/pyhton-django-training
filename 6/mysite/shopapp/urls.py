1222c4c7e28dedd285037a494dcc9ae480a59b73
d.mishin <d.mishin@rambler-co.ru>
2023-09-19 14:51:11 +0300

    made 6 task

--- 6/mysite/shopapp/urls.py
+++ 6/mysite/shopapp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import GroupsListView, ShopIndexView, products_list
+from .views import GroupsListView, ProductsListView, ShopIndexView
 
 app_name = "shopapp"
 
 urlpatterns = [
     path("", ShopIndexView.as_view(), name="shop_index"),
     path("groups/", GroupsListView.as_view(), name="groups_list"),
-    path("products/", products_list, name="products_list"),
+    path("products/", ProductsListView.as_view(), name="products_list"),
 ]
--- 6/mysite/shopapp/views.py
+++ 6/mysite/shopapp/views.py
@@ -1,8 +1,11 @@
+from typing import Any
+
 from django.contrib.auth.models import Group
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.views import View
+from django.views.generic import TemplateView
 
 from shopapp.forms import ProductForm
 
@@ -36,10 +39,20 @@ class GroupsListView(View):
 #     return render(request, "shopapp/groups-list.html", context=context)
 
 
-def products_list(request: HttpRequest):
-    context = {"products": Product.objects.all()}
+# def products_list(request: HttpRequest):
+#     context = {"products": Product.objects.all()}
 
-    return render(request, "shopapp/products-list.html", context=context)
+#     return render(request, "shopapp/products-list.html", context=context)
+
+
+class ProductsListView(TemplateView):
+    template_name = "shopapp/products-list.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["products"] = Product.objects.all()
+
+        return context
 
 
 def orders_list(request: HttpRequest):
